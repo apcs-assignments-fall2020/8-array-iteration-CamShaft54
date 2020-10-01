9b9b11ac76847c674bd985b57a807dfa1c2fdcba
camshaft54 <cam.shaw1234@gmail.com>
2020-10-01 08:44:43 -0700

    Replace Array Iteration methods with one liners

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,37 +1,27 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        int[] x = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            x[i] = arr[arr.length - 1 - i];
-        }
-        return x;
+        return IntStream.range(1, arr.length+1).boxed().mapToInt(i -> arr[arr.length - i]).toArray();
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        Arrays.sort(arr);
-        return (arr.length > 2) ? arr[arr.length-2] : -1;
+        return Arrays.stream(arr).sorted().toArray()[arr.length-2];
     }
 
     // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
-        if (arr.length >= 2) {
-            float f = (float) arr[1]/(float) arr[0];
-            for (int i = 1; i < arr.length-1; i++) {
-                if (arr[i] * f != arr[i+1]) {return false;}
-            }
-            return true;
-        } else {return false;}
+        return IntStream.range(0, arr.length-1).boxed().mapToInt(i -> (arr[i] * (float) arr[1]/(float) arr[0] == arr[i+1]) ? i : -1).noneMatch(i -> i == -1);
     }
 
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(reverse(new int[]{1, 2, 3, 4})));
         System.out.println(secondLargest(new int[]{1, 2, 3, 4}));
-        System.out.println(isGeometric(new int[]{2, 4, 8, 16}));
+        System.out.println(isGeometric(new int[]{1, 2, 4, 8, 16, 33}));
     }
 }
